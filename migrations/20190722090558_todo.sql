c72122ae8eb15aaf64fededde79b9e82ad11de21
basebandit <evansonmwangi83@gmail.com>
2019-07-23 18:02:15 +0300

    Create ToDo table schema up and down migrations

---
+++ migrations/20190722090558_todo.sql
@@ -0,0 +1,19 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE IF NOT EXISTS `ToDo` (
+		`ID` bigint(20) NOT NULL AUTO_INCREMENT,
+		`Title` varchar(200) DEFAULT NULL,
+		`Description` varchar(1024) DEFAULT NULL,
+		`Reminder` timestamp NULL DEFAULT NULL,
+		`Status` varchar(200) DEFAULT 'progress',
+		`EstimatedTimeOfCompletion` timestamp NULL DEFAULT CURRENT_TIMESTAMP, 
+		`ActualTimeOfCompletion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		PRIMARY KEY (ID),
+		UNIQUE KEY ID_UNIQUE (ID) ); 
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE `ToDo`;
+
